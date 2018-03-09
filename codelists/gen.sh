58194d17ea92e5996703ed149c34fc278b17b087
Andy Lulham <a.lulham@gmail.com>
2018-03-09 12:02:38 +0000

    Update gen.sh to remove relative paths

--- codelists/gen.sh
+++ codelists/gen.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 
-rm -rf combined-xml
-if [ -d IATI-Codelists-NonEmbedded ]; then
-    cd IATI-Codelists-NonEmbedded || exit 1
-    git checkout master
-    git pull origin master
-    cd .. || exit 1
-else
-    git clone -b master https://github.com/IATI/IATI-Codelists-NonEmbedded.git
-fi
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_PATH=$SCRIPT_PATH/..
+BUILD_PATH=$ROOT_PATH/_build
+OUTPUT_PATH=$ROOT_PATH/_output
 
-mkdir combined-xml
-cp xml/* combined-xml
+rm -rf $BUILD_PATH
+rm -rf $OUTPUT_PATH
+mkdir $BUILD_PATH
+mkdir $OUTPUT_PATH
 
-rm -rf out
-mkdir -p out/clv2/xml
+git clone -b $1 https://github.com/IATI/IATI-Codelists.git $BUILD_PATH/IATI-Codelists
+git clone -b master https://github.com/IATI/IATI-Codelists-NonEmbedded.git $BUILD_PATH/IATI-Codelists-NonEmbedded
+
+mkdir $BUILD_PATH/combined-xml
+cp $BUILD_PATH/IATI-Codelists/xml/* $BUILD_PATH/combined-xml
+
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH/clv2/xml
 
 if [ "${1:0:9}" == "version-1" ]; then
-    for f in IATI-Codelists-NonEmbedded/xml/*; do
-        python v3tov2.py $f > combined-xml/`basename $f`;
+    for f in $BUILD_PATH/IATI-Codelists-NonEmbedded/xml/*; do
+        python $SCRIPT_PATH/v3tov2.py $f > $BUILD_PATH/combined-xml/`basename $f`;
     done
-    cp combined-xml/* out/clv2/xml
+    cp $BUILD_PATH/combined-xml/* $OUTPUT_PATH/clv2/xml
 else
-    cp IATI-Codelists-NonEmbedded/xml/* combined-xml
-    mkdir -p out/clv3/xml
-    cp combined-xml/* out/clv3/xml
-    for f in combined-xml/*; do
-        python v3tov2.py $f > out/clv2/xml/`basename $f`;
+    cp $BUILD_PATH/IATI-Codelists-NonEmbedded/xml/* $BUILD_PATH/combined-xml
+    mkdir -p $OUTPUT_PATH/clv3/xml
+    cp $BUILD_PATH/combined-xml/* $OUTPUT_PATH/clv3/xml
+    for f in $BUILD_PATH/combined-xml/*; do
+        python $SCRIPT_PATH/v3tov2.py $f > $OUTPUT_PATH/clv2/xml/`basename $f`;
     done
 fi
 
-python gen.py
-python v2tov1.py
+python $SCRIPT_PATH/gen.py $OUTPUT_PATH
+python $SCRIPT_PATH/v2tov1.py $OUTPUT_PATH
 
-python mappings_to_json.py
-cp mapping.{xml,json} out/clv1/
-cp mapping.{xml,json} out/clv2/
+python $SCRIPT_PATH/mappings_to_json.py $BUILD_PATH/IATI-Codelists/mapping.xml > $OUTPUT_PATH/clv1/mapping.json
+python $SCRIPT_PATH/mappings_to_json.py $BUILD_PATH/IATI-Codelists/mapping.xml > $OUTPUT_PATH/clv2/mapping.json
+cp $BUILD_PATH/IATI-Codelists/mapping.xml $OUTPUT_PATH/clv1/
+cp $BUILD_PATH/IATI-Codelists/mapping.xml $OUTPUT_PATH/clv2/
 
 if [ "${1:0:9}" == "version-2" ]; then
-    cp mapping.{xml,json} out/clv3/
-    cp -r out/clv2/{codelists.json,codelists.xml,csv,json} out/clv3/
+    cp $OUTPUT_PATH/clv2/mapping.{xml,json} $OUTPUT_PATH/clv3/
+    cp -r $OUTPUT_PATH/clv2/{codelists.json,codelists.xml,csv,json} $OUTPUT_PATH/clv3/
 fi
